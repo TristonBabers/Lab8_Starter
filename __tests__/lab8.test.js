a54e8f683409e83051ea0b081b03d693f2b03ba3
TristonBabers <81139313+TristonBabers@users.noreply.github.com>
2022-11-21 21:27:00 -0800

    commit

--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -48,9 +48,16 @@ describe('Basic user flow for Website', () => {
     console.log('Checking the "Add to Cart" button...');
     // TODO - Step 2
     // Query a <product-item> element using puppeteer ( checkout page.$() and page.$$() in the docs )
+    let productItem = page.$("product-item");
     // Grab the shadowRoot of that element (it's a property), then query a button from that shadowRoot.
+    let shadowProdItem = productItem.shadowRoot;
+    let shadowAddCartButt = shadowProdItem.querySelector("button"); // Should return first button object it finds
     // Once you have the button, you can click it and check the innerText property of the button.
+    let beforeText = shadowAddCartButt.innerText;
+    shadowAddCartButt.click();
+    let afterText = shadowAddCartButt.innerText;
     // Once you have the innerText property, use innerText.jsonValue() to get the text value of it
+    as
   }, 2500);
 
   // Check to make sure that after clicking "Add to Cart" on every <product-item> that the Cart
